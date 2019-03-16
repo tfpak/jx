68bb58f0552da3a7127ed469526b42174e7d94db
tfpak <48423431+tfpak@users.noreply.github.com>
2019-03-16 09:00:21 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" /> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>新锐解析-全网vip视频在线解析</title>
+		<meta name="keywords" content="新锐解析,优酷VIP解析,爱奇艺VIP解析,腾讯VIP解析,乐视VIP解析,芒果VIP解析,全网视频解析,VIP在线解析,视频VIP在线解析,VIP视频在线解析,vip视频解析,vip解析,万能vip视频解析,vip视频全能解析">
+<meta name="description" content="本站为广大网友提供优酷VIP解析，爱奇艺VIP解析，腾讯VIP解析，乐视VIP解析，芒果VIP解析等解析服务，让你省去购买视频VIP费用，欢迎大家收藏本站，并将它介绍给您的朋友！">
+<link href="pucms.css" rel="stylesheet">
+<script src="jquery.min.js" type="text/javascript"></script>
+<script src="pucms.js" type="text/javascript"></script>
+
+</head>
+<body style="overflow-y:hidden;">
+<div class="panel">
+    <a href="javascript:QQ('v/a.php?url=<?php echo $_GET['url'];?>')">【A线】</a>
+    <a href="javascript:QQ('v/b.php?url=<?php echo $_GET['url'];?>')">【B线】</a>
+    <a href="javascript:QQ('v/c.php?url=<?php echo $_GET['url'];?>')">【C线】</a>
+    <a href="javascript:QQ('v/d.php?url=<?php echo $_GET['url'];?>')">【D线】</a>
+	<a href="javascript:QQ('v/f.php?url=<?php echo $_GET['url'];?>')">【E线】</a>
+    <a href="javascript:QQ('v/g.php?url=<?php echo $_GET['url'];?>')">【F线】</a>
+    <a href="javascript:QQ('v/h.php?url=<?php echo $_GET['url'];?>')">【G线】</a>
+    <a href="javascript:QQ('v/k.php?url=<?php echo $_GET['url'];?>')">【H线】</a>
+    <a href="javascript:QQ('v/w.php?url=<?php echo $_GET['url'];?>')">【K线】</a>
+    <a href="javascript:QQ('v/m.php?url=<?php echo $_GET['url'];?>')">【m3u8】</a>
+</div>
+<p class="slide">
+    <a class="WANG-WANG">更换线路</a></p>
+<div style="margin:-36px auto;width:100%;height:100%;">
+    <iframe id="WANG" scrolling="no" allowtransparency="true" frameborder="0"
+            src="v/a.php?url=<?php echo $_GET['url'];?>"width="100%" scrolling="no" height="100%" align="middle" frameborder="no" hspace="0" vspace="0" allowFullscreen="true" marginheight="0" marginwidth="0" name="tv"></iframe>
+    <script type="text/javascript"> function QQ(url) {
+            $('#WANG').attr('src', decodeURIComponent(decodeURIComponent(url))).show();
+        } </script>
+</div>
+
+
+
+<!--百度收录自动推送-->
+<script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1261349536'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1261349536' type='text/javascript'%3E%3C/script%3E"));</script>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?eb7efc1241a6113f6c30ea722b1d86cd";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+<script type="text/javascript" src="//js.users.51.la/19578927.js"></script>
+
+</div>
+</body>
+</html>
